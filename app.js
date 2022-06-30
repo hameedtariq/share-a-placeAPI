29bfc68543d6924825e355ab5bddee009ff513be
Muhammad Hameed <muhammadhameed105@gmail.com>
2022-06-30 08:44:37 +0500

    Set up CORS headers

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const app = express();
 app.use(express.urlencoded())
 app.use(express.json())
  
+// allowing CORS requests. An alternative is to use cors library
+app.use((req,res,next) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH');
+    
+    next()
+})
+
+
 
 app.get('/', (req,res)=> {
     res.send('Testing...')
